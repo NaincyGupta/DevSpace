70f2dba33eccf5883f6aa7285b06a603108e9a02
Ravi Gupta <ravigupta@Ravis-MacBook-Pro.local>
2023-05-02 17:07:58 -0700

    adding change

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const express = require('express')
+const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
+
 const app = express();
 
-//connect database
+// Connect Database
 connectDB();
 
 app.get('/',(req,res) => res.send('API running'));
 
-const PORT = process.env.PORT || 3000
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

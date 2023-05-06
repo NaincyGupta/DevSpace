9a258edb97cc10659ab2e7a7f378792c5681ac40
Ravi Gupta <ravigupta@Ravis-MacBook-Pro.local>
2023-05-05 17:20:03 -0700

    adding all files

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 // Connect Database
 connectDB();
 
-app.get('/',(req,res) => res.send('API running'));
+// Init Middleware
+app.use(express.json());
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -15,7 +16,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-const PORT = process.env.PORT || 3000;
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
